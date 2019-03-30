5da3df43dd61b2a260632807959171789d798475
Rahul Oza <oza.rahul@gmail.com>
2019-03-30 15:28:07 +0000

    Prep

---
+++ src/main/java/BCGTest/BCGTestMain.java
@@ -0,0 +1,13 @@
+package BCGTest;
+
+import java.util.logging.Logger;
+
+public class BCGTestMain {
+    final static Logger logger = Logger.getLogger(BCGTestMain.class.getName());
+    public static void main(String[] args) {
+	// write your code here
+
+        logger.info("Test Process Started Competed Successfully");
+
+    }
+}
---
+++ src/test/java/BCGTest/BCGTestMainTest.groovy
@@ -0,0 +1,30 @@
+package BCGTest
+import org.junit.Assert;
+import org.junit.Test
+import sun.nio.fs.WindowsNativeDispatcher
+
+import java.util.logging.Logger;
+
+
+class BCGTestMainTest {
+    final static Logger logger = Logger.getLogger(BCGTestMain.class.getName());
+    void setUp() {
+        super.setUp()
+    }
+
+    void tearDown() {
+    }
+
+    @Test
+    void testMain() {
+
+        logger.info("Test  Started Competed Successfully");
+        /*Assert.assertEquals(0d, account.getOverdraftLimit(), epsilon);
+        Assert.assertFalse(account.deposit(depAmt));
+        Assert.assertFalse(account.withdraw(depAmt));
+        Assert.assertFalse(account.withdraw(-(overDraftLim+1)));
+        Assert.assetSame(account.)*/
+
+
+    }
+}
